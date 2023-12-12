db11ebc4bc9f1b78afbb7395b2875eb0de0d27be
Raman Preet Singh <ramanpre0810@gmail.com>
2023-12-13 01:25:05 +0530

    Day 4 completed

--- create_day.sh
+++ create_day.sh
@@ -12,9 +12,25 @@ folder_name="src/day_$day_number"
 mkdir -p "$folder_name"
 
 # Create the main.rs file
-echo "fn main() {" >> "$folder_name/main.rs"
-echo '	println!("Hello from day_'"$day_number"'!");' >> "$folder_name/main.rs"
-echo "}" >> "$folder_name/main.rs"
+echo "use aoc2023::{get_file_path, read_file};
+use std::str::Lines;
+
+fn part_1(lines: Lines) -> i32 {
+    0
+}
+
+fn part_2(lines: Lines) -> i32 {
+    0
+}
+
+fn main() {
+    let is_sample = true;
+    let path = get_file_path($day_number, is_sample);
+    let contents = read_file(path);
+
+    println!("Part 1: {}", part_1(contents.lines()));
+    println!("Part 2: {}", part_2(contents.lines()));
+}" >> "$folder_name/main.rs"
 
 #creating input files
 touch "$folder_name/sample.txt"
@@ -22,7 +38,7 @@ touch "$folder_name/input.txt"
 
 
 # Update the Cargo.toml file
-echo "" >> Cargo.toml 
+echo "" >> Cargo.toml
 echo "[[bin]]" >> Cargo.toml
 echo "name = \"day_$day_number\"" >> Cargo.toml
 echo "path = \"$folder_name/main.rs\"" >> Cargo.toml
